f8b44a138a7d931603ffd94f8dc1918274b9ec93
liuhui <huiliu233@gmail.com>
2022-05-30 21:31:49 +0800

    update result.html

---
+++ parse_data.py
@@ -0,0 +1,49 @@
+import json
+import tarfile
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+# #%%
+#
+# metadata = pd.read_csv("2020-04-03/metadata_with_mag_mapping_04_03.csv", encoding="utf-8")
+# metadata.head()
+#
+# #%%
+#
+# # find by paper id
+# metadata.query("sha == 'f056da9c64fbf00a4645ae326e8a4339d015d155'")
+#
+# #%%
+#
+# # find by title
+# metadata.query("title == 'SIANN: Strain Identification by Alignment to Near Neighbors'")
+#
+# #%% md
+#
+# tar = tarfile.open("2020-04-03/biorxiv_medrxiv.tar.gz", "r:gz")
+# all_data = []
+# for tarinfo in tqdm(tar):
+#     if tarinfo.isreg():
+#         f = tar.extractfile(tarinfo.name)
+#         data = json.loads(f.read())
+#         all_data.append(data)
+#         print(data)
+#
+# with open('data.json', "w", encoding="utf-8") as f:
+#         json_str = json.dump(all_data, f, indent=4, ensure_ascii=False)
+
+meta_data = {}
+with open('data.json', "r", encoding="utf-8") as f:
+    all_data = json.load(f)
+    for paper in all_data:
+        paper_id = paper['paper_id']
+        title = paper['metadata']['title']
+        if len(paper['abstract']):
+            abstract = paper['abstract'][0]['text']
+        else:
+            abstract = []
+        meta_data[paper_id] = {'title': title, 'abstract': abstract}
+
+with open('meta_data.json', "w", encoding="utf-8") as f:
+    json.dump(meta_data, f, indent=4, ensure_ascii=False)
---
+++ search/BooleanSearch.py
@@ -0,0 +1,54 @@
+import json
+from utlis import preprocess, middle_to_after, st
+
+class BooleanSearch:
+    def __init__(self):
+        with open('../index/inverted_index.json', 'r', encoding='utf-8') as f:
+            self.posting_lists = json.load(f)
+        with open('../meta_data.json', 'r', encoding='utf-8') as f:
+            self.meta_data = json.load(f)
+
+    def search(self, query):
+        expression = middle_to_after(query)
+        modified_expression = []
+        for item in expression:
+            if item not in ['NOT', 'AND', 'OR']:
+                item = st.stem(item)
+                item = set(self.posting_lists[item].keys())
+            modified_expression.append(item)
+
+        stack_value = []
+        for idx, item in enumerate(modified_expression):
+            if item in ['NOT', 'AND', 'OR']:
+                n2 = stack_value.pop()
+                n1 = stack_value.pop()
+                result = n1
+                if item == 'NOT':
+                    result -= n2
+                elif item == 'AND':
+                    result &= n2
+                elif item == 'OR':
+                    result |= n2
+                stack_value.append(result)
+            else:
+                stack_value.append(item)  # 词语直接压栈
+        result = list(stack_value[0])
+        
+        for i in range(len(result)):
+            uid = result[i]
+            result[i] = self.meta_data[uid]
+            print(result[i])
+
+        return result
+
+if __name__ == '__main__':
+    bool_search = BooleanSearch()
+    query = "Strain AND Identification OR network"
+    print('Query: ', query)
+    bool_search.search(query)
+    print("====================================================================")
+    query = "Strain AND Identification OR network NOT automated"
+    print('Query: ', query)
+    bool_search.search(query)
+
+

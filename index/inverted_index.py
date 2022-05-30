736e062c5af1cab19d4a58d6af1d2decea3c6945
liuhui <huiliu233@gmail.com>
2022-05-30 22:33:53 +0800

    add search

--- index/inverted_index.py
+++ index/inverted_index.py
@@ -1,10 +1,43 @@
 import json
-from utils import preprocess
+import sys
+sys.path.append("..")
+from utlis import preprocess
+import pandas as pd
+import pickle
+from tqdm import tqdm
+
+def construct_inverted_index_from_metadata(fussy_method=None):
+    # print("nothing OK")
+    metadata = pd.read_csv("../2020-04-03/metadata_with_mag_mapping_04_03.csv", encoding="utf-8")
+    # print("Open OK")
+    inverted_index_map = {}
+    for idx, paper in tqdm(metadata.iterrows()):
+        cord_uid = paper['cord_uid']
+        paper_id = paper['sha']
+        title = preprocess(paper['title'], fussy_method=fussy_method)
+        abstract = preprocess(paper['abstract'], fussy_method=fussy_method)
+        tokens = title + abstract
+        # print(tokens)
+        for token in tokens:
+            if token in inverted_index_map.keys():
+                posting_list = inverted_index_map[token]
+                if cord_uid in posting_list.keys():
+                    inverted_index_map[token][cord_uid] += 1
+                else:
+                    inverted_index_map[token][cord_uid] = 1
+            else:
+                inverted_index_map[token] = {cord_uid: 1}
+    # print(inverted_index_map)
+    # print("OK")
+    with open(f'../data/inverted_index_{fussy_method}.pkl', 'wb') as f:
+        pickle.dump(inverted_index_map, f)
+
 
 def construct_inverted_index(fussy_method=None):
-    with open('../data.json', encoding='utf-8') as f:
+    # print("nothing OK")
+    with open('./data/data.json', encoding='utf-8') as f:
         all_data = json.load(f)
-
+    # print("Open OK")
     inverted_index_map = {}
     for paper in all_data:
         paper_id = paper['paper_id']
@@ -14,7 +47,7 @@ def construct_inverted_index(fussy_method=None):
         else:
             abstract = []
         tokens = title + abstract
-        print(tokens)
+        # print(tokens)
         for token in tokens:
             if token in inverted_index_map.keys():
                 posting_list = inverted_index_map[token]
@@ -25,11 +58,11 @@ def construct_inverted_index(fussy_method=None):
             else:
                 inverted_index_map[token] = {paper_id: 1}
     # print(inverted_index_map)
-
-    with open(f'inverted_index_{fussy_method}.json', 'w', encoding='utf-8') as f:
+    # print("OK")
+    with open(f'./data/inverted_index_{fussy_method}.json', 'w', encoding='utf-8') as f:
         json.dump(inverted_index_map, f,  indent=4, ensure_ascii=False)
 
 if __name__ == '__main__':
-    construct_inverted_index(fussy_method='none')
-    construct_inverted_index(fussy_method='stem')
-    construct_inverted_index(fussy_method='lemmatize')
+    construct_inverted_index_from_metadata(fussy_method='none')
+    # construct_inverted_index(fussy_method='stem')
+    # construct_inverted_index(fussy_method='lemmatize')
---
+++ test.py
@@ -0,0 +1,18 @@
+import citation
+import dataclass
+import utils
+data = dataclass.dataclass("stem")
+print(data.id_to_title("daf32e013d325a6feb80e83d15aabc64a48fae33"))
+title = dataclass.str_to_tuple("Spatial epidemiology of networked metapopulation: An overview","stem")
+print(title)
+print(data.title_to_id(title))
+num_with_citation = 0
+num_sited = 0
+for i in data.d_cites:
+    if len(data.d_cites[i])>0:
+        num_with_citation += 1
+    if len(data.d_cited_by[i])>0:
+        num_sited += 1
+
+print(num_with_citation,"/",len(data.d_cites))
+print(num_sited,"/",len(data.d_cited_by))
